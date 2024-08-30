b71c4e56a0bfacd68c43eb85ff3b4927978bdcfe
CarlosSotoDev <cssottog@gmail.com>
2024-08-29 21:27:02 -0600

    Se actualizo rama features/Carlos yse puso al día

--- index.php
+++ index.php
@@ -313,7 +313,7 @@ include('includes/config.php');
                 <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#tramitesAccordion">
                     <div class="card-body">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.
+                        labore et dolore magna aliua.
                     </div>
                 </div>
             </div>
